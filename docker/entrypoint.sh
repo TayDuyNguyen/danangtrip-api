cb359be172c8a1ac3c5aef9bd96b82f43f4efee7
Tay Nguyen <goline.tay@gmail.com>
2026-04-20 18:06:56 +0700

    fix: docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,8 @@ set -eu
 
 cd /var/www
 
+PORT="${PORT:-10000}"
+
 if [ -f composer.json ]; then
   if [ ! -f vendor/autoload.php ]; then
     composer install --no-interaction --prefer-dist
@@ -12,5 +14,12 @@ if [ -f composer.json ]; then
     composer install --no-interaction --prefer-dist
   fi
 fi
+
+if [ -f /etc/nginx/templates/default.conf.template ]; then
+  envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+fi
+
+mkdir -p /run/php
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 
 exec "$@"
